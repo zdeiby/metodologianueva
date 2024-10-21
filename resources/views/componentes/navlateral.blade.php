87809313d941ac9d68ee32e6cff4e63241dbf4f3
deiby <deibygj@hotmail.com>
2024-10-21 08:09:43 -0500

    adsense

--- resources/views/componentes/navlateral.blade.php
+++ resources/views/componentes/navlateral.blade.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@yield('title')</title>
     <meta name="csrf-token" content="{{ csrf_token() }}">
+    <meta name="google-adsense-account" content="ca-pub-9423200751583677">
      <link href="{{ asset('assets/bootstrap/bootstrap.css') }}" rel="stylesheet" > 
      <link rel="shortcut icon" href="../../favicon.ico">
      <link href="{{ asset('resources/sweetalert/sweetalert2.min.css') }}" rel="stylesheet" > 
