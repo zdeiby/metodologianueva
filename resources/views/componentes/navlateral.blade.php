ffae93b47417de62cd21ef4203ba2aa898874057
deiby <deibygj@hotmail.com>
2024-10-30 16:36:19 -0500

    textarea

--- resources/views/componentes/navlateral.blade.php
+++ resources/views/componentes/navlateral.blade.php
@@ -745,8 +745,9 @@ cloud.addEventListener("click",()=>{
 
 function validateInput(textarea) {
     // Expresión regular para permitir solo letras con tildes, comas, puntos, punto y coma, guiones, diagonales y saltos de línea
-    const regex = /^[a-zA-ZáéíóúÁÉÍÓÚñÑ ,.;/\-\n]*$/;
+    const regex = /^[a-zA-Z0-9áéíóúÁÉÍÓÚñÑ ,.;/\-\n]*$/;
 
+    textarea.value = textarea.value.toUpperCase();
     // Validar el texto ingresado
     if (!regex.test(textarea.value)) {
         // Elimina el último carácter si no coincide con la expresión regular
