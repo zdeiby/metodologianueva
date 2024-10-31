0809b7f02eb0017056d219ac08c99be5789ff238
deiby <deibygj@hotmail.com>
2024-10-31 10:29:18 -0500

    primary keys sincro

--- app/Http/Controllers/c_sincronizacion.php
+++ app/Http/Controllers/c_sincronizacion.php
@@ -6,7 +6,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\URL;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Http;
-use App\Http\ComplementosControllers\Sql;
+use App\Http\ComplementosControllers\sql;
 
 class c_sincronizacion extends Controller
 {
